c11fb2716d145bc40c471a869379088e8ba65485
Nikhilvedi <nv94@hotmail.co.uk>
2017-04-01 17:52:28 +0100

    adding testing

---
+++ test/test.js
@@ -0,0 +1,44 @@
+"use strict";
+// NPM install mongoose and chai. Make sure mocha is globally
+// installed
+const mongoose = require('mongoose');
+var mongoOp = require("../model/mongo");
+const chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../routes/index.js');
+var should = chai.should();
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+mongoose.Promise = global.Promise;
+
+  describe('Lock Authentication Collection', function() {
+    it('Save user schema to database', function(done) {
+      var db = new mongoOp.userLogin({
+          name: '1@test.com',
+          password: 'P@ssword123',
+          LockID: 'x2353',
+          firstname: 'John'
+      });
+      // save the user
+      db.save(function(err) {
+          if (err) {
+                done(err);
+              } else {
+                    done();
+                  }
+          });
+      });
+    });
+
+// describe('Check Lock Requests', function() {
+//   it('Check Lock Requests', function(done) {
+//   chai.request(server)
+//     .get('/Lock')
+//     .end(function(err, res){
+//       res.should.have.status(200);
+//       res.should.be.json;
+//       done();
+//     });
+// });
+// })
